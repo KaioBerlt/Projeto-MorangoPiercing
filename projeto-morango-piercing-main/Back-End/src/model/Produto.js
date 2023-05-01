addfc2eaf566c567ec6ef5b814ae1e08731cb1c8
KaioBerlt <kaioberlt@gamil.com>
2023-04-30 22:14:55 -0300

    Fix produto model

--- projeto-morango-piercing-main/Back-End/src/model/Produto.js
+++ projeto-morango-piercing-main/Back-End/src/model/Produto.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
 const ProdutoSchema = new mongoose.Schema({
-    nome: {type: String, unique: true, required: true},
+    nome: {type: String, required: true},
     descricao: { type: String, required: true},
     precoUnitario: { type: Number, required: true},
     imagem: { type: String, required: true},
     codigoBarra: { type: Number, unique: true, required: true},
     categorias: [
         {
+            
             _id: { type: mongoose.Schema.Types.ObjectId, required: true, unique: true, ref: "categorias"},
             createdAt: { type: Date, required: true, default: Date.now() }
         },
